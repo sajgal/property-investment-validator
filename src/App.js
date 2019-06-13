28a40bd684e96dec964f351b25ed8b70890fc7b8
Matej Sajgal <roarbb@gmail.com>
2019-06-13 21:54:47 +1000

    Add basic antd form with one input and a submit button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/antd.css';
+import styled from 'styled-components';
+
+import InvestmentForm from './InvestmentForm';
+
+const Wrapper = styled.section`
+  width: 960px;
+  margin: 4em auto;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <InvestmentForm />
+    </Wrapper>
   );
 }
 
