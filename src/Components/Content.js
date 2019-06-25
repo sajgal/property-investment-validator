526e13b065b35de2ae5bc155b8dfdf1dc58dcbe7
Matej Sajgal <roarbb@gmail.com>
2019-06-25 17:17:29 +1000

    Replaced antd grid with css grid

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,15 +1,40 @@
+import { Layout } from 'antd';
 import React from 'react';
-import { Layout, Row, Col } from 'antd';
+import styled from 'styled-components';
+
 import InvestmentForm from './InvestmentForm';
 import Results from './Results';
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 300px auto;
+  grid-gap: 15px;
+
+  @media (max-width: 768px) {
+    grid-auto-flow: row;
+    grid-template-columns: auto;
+  }
+`;
+
+const GridResults = styled.div`
+  @media (max-width: 768px) {
+    order: 2;
+  }
+`;
+
+const GridForm = styled.div`
+  @media (max-width: 768px) {
+    order: 1;
+  }
+`;
 
 const Content = () => {
   return (
     <Layout.Content style={{ padding: '50px' }}>
-      <Row>
-        <Col sm={24} md={5}><Results /></Col>
-        <Col sm={24} md={19}><InvestmentForm /></Col>
-      </Row>
+      <Grid>
+        <GridResults><Results /></GridResults>
+        <GridForm><InvestmentForm /></GridForm>
+      </Grid>
     </Layout.Content>
   );
 };
