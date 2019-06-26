ad4418adfa66cc4b763d94eec439ef872f259423
Matej Sajgal <roarbb@gmail.com>
2019-06-26 20:06:52 +1000

    Header title font + responsivity

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,20 @@ import { Layout } from 'antd';
 import styled from 'styled-components';
 
 const HeaderTitle = styled.h1`
-  color: #fff
+  color: #fff;
+  @import url('https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz');
+  font-family: 'Yanone Kaffeesatz', sans-serif;
+  font-size: 2em;
+  white-space: nowrap;
+
+  @media (max-width: 400px) {
+    font-size: 1.5em;
+  }
+
+  @media (max-width: 250px) {
+    font-size: 1.2em;
+  }
+
 `;
 
 const Header = () => {
